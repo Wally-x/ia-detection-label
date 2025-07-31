0da369445db9ff2602e4218c9f99f70836abc08a
Quentin Chevalier <quentin-chevalier@planet.local>
2025-07-31 11:38:00 +0200

    Change .venv

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
 if __name__ == "__main__":
-    pass
+    import torch
+    import torchvision
+    import optuna
+    import albumentations
+
+    print("Torch:", torch.__version__)
+    print("CUDA dispo ?", torch.cuda.is_available())
